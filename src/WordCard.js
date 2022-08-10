066eee009152f50834e2518e46ae27c5f4dcb95b
Setthaphon Pruckdakorn <thueanz@gmail.com>
2022-08-11 00:30:17 +0700

    3.Change Condition

--- src/WordCard.js
+++ src/WordCard.js
@@ -37,15 +37,15 @@ export default function WordCard(props){
         let guess = state.guess + c
         setState({...state, guess})
 
-        if(guess.length == state.word.length){
-            if(guess == state.word){
+        //if(guess.length == state.word.length){
+            if(guess == state.Largest){
                 console.log('yeah!')
                 setState({...state, guess: '', completed: true})
             }else{
                 console.log('reset')
                 setState({...state, guess: '', attempt: state.attempt + 1})
             }   
-        } 
+        //} 
     }
 
     return (
