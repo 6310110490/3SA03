57756e3a3c32bcc3de1e4e7f0e09173860db6199
Setthaphon Pruckdakorn <thueanz@gmail.com>
2022-08-11 00:47:39 +0700

    5.Add Description

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "12345";
+const word2 = "65279";
+const word3 = "07652";
 function App() {
     return (
         <div>
+            <h1>Find the largest number</h1>
+            <h3>How to play</h3>
+            <p>You must choose the largest number in each row</p>
             <WordCard value={word}/>
+            <WordCard value={word2}/>
+            <WordCard value={word3}/>
         </div>
     );
 }
